436e0b5dca1a0078313417aec31dabc198b34e40
Aftab1112 <you@example.com>
2024-05-20 09:19:22 +0530

    completed refund page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import CourseCurriclumPage from "./components/CourseCurriclumPage";
 import Home from "./components/Home";
+import RefundOffer from "./components/RefundOffer";
 
 const App = () => {
   return (
     <div className="App">
       <Home />
       <CourseCurriclumPage />
+      <RefundOffer />
     </div>
   );
 };
